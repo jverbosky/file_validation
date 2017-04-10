96a69d79a04a1275a937cf11fc9a42b6f1c13c83
jverbosk <jverbosk@gmail.com>
2017-04-10 11:56:18 -0400

    initial upload, started reading binary data from JPG files

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+# require 'fileutils'
+require_relative "file_validation.rb"
+# include FileUtils::Verbose
+
+get '/' do
+    erb :upload
+end
+
+post '/' do
+    # "#{params}"  # {"file"=>{:filename=>"upload_1.png", :type=>"image/png", :name=>"file", :tempfile=>#, :head=>"Content-Disposition: form-data; name=\"file\"; filename=\"upload_1.png\"\r\nContent-Type: image/png\r\n"}}
+    # tempfile = params[:file][:tempfile]
+    # filename = params[:file][:filename]
+    file_hash = params[:file]
+    file_type = file_type(file_hash)
+    "#{file_type}"
+end
---
+++ file_validation.rb
@@ -0,0 +1,28 @@
+file_names = {
+  jpg: ["jpg", "JPG", "jpe", "JPE", "jpeg", "JPEG"]
+}
+
+file_data = {
+  jpg: "xff\xd8\xff\xe0"
+}
+
+def jpg?(filename, data)
+  # return status == (filename[:jpg] && data[0,4] == file_data[:jpg])
+end
+
+def file_type(file_hash)
+  response = "Not a valid image file."
+  response = "File is jpg." if jpg?(file_hash[:filename], file_hash[:tempfile])
+  return response
+end
+
+data = File.binread("./flood-damage.jpg")[0, 4]
+p data  # "\xFF\xD8\xFF\xE0"
+
+data = File.binread("./borregoflooddamgage.JPG")[0, 4]
+p data  # "\xFF\xD8\xFF\xE1"
+
+data = File.binread("./renamed_exe.jpg")[0, 4]
+p data  # "MZ\x90\x00"
+
+
